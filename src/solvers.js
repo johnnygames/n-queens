192ea4c5a3ae1725c59c539b6551520c65415daf
Johnnygames <johnegames@gmail.com>
2015-02-12 16:01:18 -0800

    Implemented countNRooksSolutions method

--- src/solvers.js
+++ src/solvers.js
@@ -14,18 +14,62 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //store the inital matrix in a variable
+  var solutionBoard = new Board({"n": n});
+  //define recursive function (rowIndex)
+  var recursiveHelper = function (rIndex) {
+    if (rIndex > (n - 1) ) {
+      return solutionBoard;
+    }
+    for (var j = 0; j < n; j++) {
+      solutionBoard.togglePiece(rIndex, j);
+      if (!solutionBoard.hasAnyRowConflicts() && !solutionBoard.hasAnyColConflicts()) {
+         return recursiveHelper(rIndex+1);        
+      } else {
+        solutionBoard.togglePiece(rIndex, j);
+        continue;
+      }
+    }
+  };
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var solution = recursiveHelper(0);
+  //iterate through the single row/array
+    //run togglePiece on each value and run col and row conflict tests
+    //if passes, test for which row currently in
+      //if that row is (n - 1) then finished and return matrix
+      //if not last row, recurse
+
+  var board = solution.rows();
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return board;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
 
+  var solutionBoard = new Board({"n": n});
+  var solutionCount = 0;
+
+  var recursiveHelper = function (rIndex) {
+    if (rIndex > (n - 1) ) {
+      return;
+    }
+    for (var j = 0; j < n; j++) {
+      solutionBoard.togglePiece(rIndex, j);
+      if (!solutionBoard.hasAnyRowConflicts() && !solutionBoard.hasAnyColConflicts()) {
+         if (rIndex === n - 1) {
+            solutionCount++;
+         }
+         recursiveHelper(rIndex+1);        
+      } 
+        solutionBoard.togglePiece(rIndex, j);
+    }
+  };
+
+  recursiveHelper(0);
+  
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
